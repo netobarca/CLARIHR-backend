deaa69ea57ce7e222c996bb2b6023803d7ba5097
Christopher Cañas <neto.barca2@gmail.com>
2026-05-24 19:48:54 -0600

    feat(worker-load-tests): add load testing harness for PDF rendering performance

--- src/CLARIHR.Infrastructure/Properties/AssemblyInfo.cs
+++ src/CLARIHR.Infrastructure/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("CLARIHR.Application.UnitTests")]
 [assembly: InternalsVisibleTo("CLARIHR.Api.IntegrationTests")]
+[assembly: InternalsVisibleTo("CLARIHR.Worker.LoadTests")]
