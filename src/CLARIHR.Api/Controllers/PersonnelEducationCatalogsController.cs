0fb6cffc245ee5c8318c066d81b34b058a5fb15c
Christopher Cañas <neto.barca2@gmail.com>
2026-06-04 21:57:54 -0600

    feat: Remove PersonnelEducationCatalogsController as it is now obsolete

--- src/CLARIHR.Api/Controllers/PersonnelEducationCatalogsController.cs
+++
@@ -1,4 +0,0 @@
-// This file is intentionally left empty.
-// Personnel education catalogs are now global system catalogs.
-// Use EducationCatalogsController instead.
-// See: src/CLARIHR.Api/Controllers/EducationCatalogsController.cs
